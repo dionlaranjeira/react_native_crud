7cb422ca9d3239b2a1a96434202d7da872e61efa
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-06-01 16:28:41 -0400

    ListItem config
    Add Avatar, Title and SubTitle.

---
+++ src/data/users.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 1,
+        name: "Dion Ribeiro Laranjeira",
+        email: "dionribeiro.rr@gmail.com",
+        avatarUrl: "https://avatars.githubusercontent.com/u/54155542?v=4",
+    },
+    {
+        id: 2,
+        name: "Matheus Carvalho",
+        email: "matheus9126@gmail.com",
+        avatarUrl: "https://avatars.githubusercontent.com/u/55814214?v=4",
+    },
+    {
+        id: 3,
+        name: "Jeferson Macedo",
+        email: "jefersonmacedowgt@gmail.com",
+        avatarUrl: "https://avatars.githubusercontent.com/u/61505613?v=4",
+    },
+
+]
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,47 @@
 import React from "react";
-import {Text} from "react-native";
+import {Alert, Text} from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
+import { ListItem, Avatar } from 'react-native-elements'
 
+
+
+import users from "../data/users"
 export default props =>{
+    
+    function getUserItem({item: user}){
+        return (
+            <View>
+                <ListItem
+                bottomDivider
+                onPress={()=>props.navigation.navigate('UserForm')}
+                key={user.id}>
+                    <Avatar rounded size="large" source={{uri: user.avatarUrl}} />
+                    <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+            </View>
+        )
+    }
+    
     return(
-        <Text>UserList</Text>
+       <View>
+           <FlatList
+           data= {users}
+           keyExtractor={user => user.id.toString()}
+           renderItem={getUserItem}
+           >
+
+           </FlatList>
+       </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+     avatar:{
+         borderRadius: 10,
+     }
+    });
+    
