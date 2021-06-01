b59d4bfcb424ff76e276c29962fe450677c6bf09
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-05-31 20:35:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react";
-import {Text,SafeAreaView} from "react-native";
-import UserForms from "./views/UserForm";
+import {Text} from "react-native";
 import UserList from "./views/UserList";
+import UserForm from "./views/UserForm";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default props =>{
     return(
-        <>
-        <SafeAreaView>
-         <UserForms></UserForms>
-         <UserList></UserList>
-        </SafeAreaView>
-        </>
+        <NavigationContainer>
+            <Stack.Navigator
+            initialRouteName="UserList"
+            >
+            <Stack.Screen
+            name="UserList"
+            component={UserList}
+            />
+            <Stack.Screen
+            name="UserForm"
+            component={UserForm}
+            />   
+            </Stack.Navigator>
+        </NavigationContainer>
+        
     );
 }
